29e362d2f38ffc37d90bb7282eedb717139718d1
Samuel Han <sh779@cornell.edu>
2020-07-24 05:50:24 -0400

    Adding csrf to forms and js

--- health_app/templates/health/vis.js
+++ health_app/templates/health/vis.js
@@ -1,3 +1,13 @@
+var csrf_token = "{{ csrf_token() }}";
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrf_token);
+        }
+    }
+});
+
 var canvas = document.getElementById("myChart");
 var ctx = canvas.getContext('2d');
 

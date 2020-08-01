09f5f618a55d48ae27e717187a9bb6ecabc5ad1c
Samuel Han <sh779@cornell.edu>
2020-08-01 15:50:06 -0400

    Added feature to move around graph

--- health_app/templates/health/vis.js
+++ health_app/templates/health/vis.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("myChart");
 var ctx = canvas.getContext('2d');
 
 /*
- * Used to get start of the current week
+ * Use this function to get the start of this current week
  */
 function getSundayDate(d) {
     d = new Date(d);
@@ -11,8 +11,8 @@ function getSundayDate(d) {
     return new Date(d.setDate(diff))
 };
 
-/*
- * Used to store cookies for website usage
+/*     
+ * Use this function to create initial chart for week and then store other chart type cookie for session use
  */
 function getCookie(name) {
     var dc = document.cookie;
@@ -34,7 +34,177 @@ function getCookie(name) {
 }
 
 /*
- * Creates visualization charts
+ * Gets week option and uses n to decide how far to move
+ */
+function getWeekOptions(n) {
+    // Used to get this weeks' dates, and sets week option format
+    let minDate = getSundayDate(new Date());
+    minDate.setDate(minDate.getDate()+(7*n));
+    let minDateOnly = (minDate.getMonth()+1) + "/" + minDate.getDate() + "/" + minDate.getFullYear();
+    minDate.setDate(minDate.getDate() + 6); 
+    let maxDateOnly = (minDate.getMonth()+1) + "/" + minDate.getDate() + "/" + minDate.getFullYear();
+    console.log(minDateOnly);
+    console.log(maxDateOnly);
+    let weekOption = [{
+        type: "time",
+        time: {
+            parser: "MM/DD/YYYY",
+            tooltipFormat: "ll",
+            unit: "day",
+            displayFormats: {
+                "day": "MM/DD (dddd)"
+            },
+            min: minDateOnly,
+            max: maxDateOnly
+        },
+        scaleLabel: {
+            display: true,
+            labelString: 'Date'
+        }
+    }];
+    
+    // Chart options for week
+    let chartOptions = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    suggestedMin: 100,
+                    suggestedMax: 200
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Weight (lbs)'
+                }
+            }],
+            xAxes: weekOption
+        },
+        legend: {
+            display: false,
+            position: "right",
+            align: "end",
+        }
+    };
+
+    return chartOptions;
+}
+
+/*
+ * Gets month option and uses n to decide how far to move
+ */
+function getMonthOptions(n) {
+    // Calculates this month's dates, and sets month option format
+    let date = new Date();
+    let minMonthDate = new Date();
+    let maxMonthDate = new Date();
+    minMonthDate.setFullYear(date.getFullYear(), date.getMonth()+n, 1);
+    maxMonthDate.setFullYear(date.getFullYear(), date.getMonth() + 1 + n, 0);
+    let minMonthDateOnly = (minMonthDate.getMonth()+1) + "/" + minMonthDate.getDate() + "/" + minMonthDate.getFullYear();
+    let maxMonthDateOnly = (maxMonthDate.getMonth()+1) + "/" + maxMonthDate.getDate() + "/" + maxMonthDate.getFullYear();
+    let monthOption = [{
+        type: "time",
+        time: {
+            parser: "MM/DD/YYYY",
+            tooltipFormat: "ll",
+            unit: "day",
+            displayFormats: {
+                "day": "MM/DD"
+            },
+            min: minMonthDateOnly,
+            max: maxMonthDateOnly
+        },
+        scaleLabel: {
+            display: true,
+            labelString: 'Date'
+        }
+    }];
+    
+    //Chart options for month
+    let chartMonthOptions = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    suggestedMin: 100,
+                    suggestedMax: 200
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Weight (lbs)'
+                }
+            }],
+            xAxes: monthOption
+        },
+        legend: {
+            display: false,
+            position: "right",
+            align: "end",
+        }
+    };
+
+    return chartMonthOptions;
+}
+
+/*
+ * Gets year option and uses n to decide how far to move
+ */
+function getYearOptions(n) {
+    //Calculates dates for this year, and sets this year option format
+    let date = new Date();
+    let minYearDate = new Date();
+    let maxYearDate = new Date();
+    minYearDate.setFullYear(date.getFullYear(), 0, 1);
+    maxYearDate.setFullYear(date.getFullYear(), 11, 31);
+    let minMonthDate = new Date();
+    let maxMonthDate = new Date();
+    minMonthDate.setFullYear(date.getFullYear()+n, date.getMonth(), 1);
+    maxMonthDate.setFullYear(date.getFullYear()+n, date.getMonth() + 1, 0);
+    let minYearDateOnly = (minYearDate.getMonth()+1) + "/" + minYearDate.getDate() + "/" + minMonthDate.getFullYear();
+    let maxYearDateOnly = (maxYearDate.getMonth()+1) + "/" + maxYearDate.getDate() + "/" + maxMonthDate.getFullYear();
+    let yearOption = [{
+        type: "time",
+        time: {
+            parser: "MM/DD/YYYY",
+            tooltipFormat: "ll",
+            unit: "month",
+            stepSize: 1,
+            displayFormats: {
+                "day": "MMM"
+            },
+            min: minYearDateOnly,
+            max: maxYearDateOnly
+        },
+        scaleLabel: {
+            display: true,
+            labelString: 'Date'
+        }
+    }];
+    
+    // Chart options for year
+    let chartYearOptions = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    suggestedMin: 100,
+                    suggestedMax: 200
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Weight (lbs)'
+                }
+            }],
+            xAxes: yearOption
+        },
+        legend: {
+            display: false,
+            position: "right",
+            align: "end",
+        }
+    };
+
+    return chartYearOptions;
+}
+
+/*
+ * Creates ChartJS with given parameters and utilizes cookies to save past data
  */
 function createChart() {
     // Chart data from DB (passed via render_template)
@@ -62,146 +232,6 @@ function createChart() {
         ]
     };
 
-    // Used to get this weeks' dates, and sets week option format
-    let minDate = getSundayDate(new Date());
-    let minDateOnly = (minDate.getMonth()+1) + "/" + minDate.getDate() + "/" + minDate.getFullYear();
-    minDate.setDate(minDate.getDate() + 6); 
-    let maxDateOnly = (minDate.getMonth()+1) + "/" + minDate.getDate() + "/" + minDate.getFullYear();
-    let weekOption = [{
-        type: "time",
-        time: {
-            parser: "MM/DD/YYYY",
-            tooltipFormat: "ll",
-            unit: "day",
-            displayFormats: {
-                "day": "MM/DD (dddd)"
-            },
-            min: minDateOnly,
-            max: maxDateOnly
-        },
-        scaleLabel: {
-            display: true,
-            labelString: 'Date'
-        }
-    }];
-
-    // Calculates this month's dates, and sets month option format
-    let date = new Date();
-    let minMonthDate = new Date();
-    let maxMonthDate = new Date();
-    minMonthDate.setFullYear(date.getFullYear(), date.getMonth(), 1);
-    maxMonthDate.setFullYear(date.getFullYear(), date.getMonth() + 1, 0);
-    let minMonthDateOnly = (minMonthDate.getMonth()+1) + "/" + minMonthDate.getDate() + "/" + minMonthDate.getFullYear();
-    let maxMonthDateOnly = (maxMonthDate.getMonth()+1) + "/" + maxMonthDate.getDate() + "/" + maxMonthDate.getFullYear();
-    let monthOption = [{
-        type: "time",
-        time: {
-            parser: "MM/DD/YYYY",
-            tooltipFormat: "ll",
-            unit: "day",
-            displayFormats: {
-                "day": "MM/DD"
-            },
-            min: minMonthDateOnly,
-            max: maxMonthDateOnly
-        },
-        scaleLabel: {
-            display: true,
-            labelString: 'Date'
-        }
-    }];
-
-    //Calculates dates for this year, and sets this year option format
-    let minYearDate = new Date();
-    let maxYearDate = new Date();
-    minYearDate.setFullYear(date.getFullYear(), 0, 1);
-    maxYearDate.setFullYear(date.getFullYear(), 11, 31);
-    let minYearDateOnly = (minYearDate.getMonth()+1) + "/" + minYearDate.getDate() + "/" + minMonthDate.getFullYear();
-    let maxYearDateOnly = (maxYearDate.getMonth()+1) + "/" + maxYearDate.getDate() + "/" + maxYearDate.getFullYear();
-    let yearOption = [{
-        type: "time",
-        time: {
-            parser: "MM/DD/YYYY",
-            tooltipFormat: "ll",
-            unit: "month",
-            stepSize: 1,
-            displayFormats: {
-                "day": "MMM"
-            },
-            min: minYearDateOnly,
-            max: maxYearDateOnly
-        },
-        scaleLabel: {
-            display: true,
-            labelString: 'Date'
-        }
-    }];
-
-    // Chart options for week
-    let chartOptions = {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    suggestedMin: 100,
-                    suggestedMax: 200
-                },
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Weight (lbs)'
-                }
-            }],
-            xAxes: weekOption
-        },
-        legend: {
-            display: false,
-            position: "right",
-            align: "end",
-        }
-    };
-
-    //Chart options for month
-    let chartMonthOptions = {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    suggestedMin: 100,
-                    suggestedMax: 200
-                },
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Weight (lbs)'
-                }
-            }],
-            xAxes: monthOption
-        },
-        legend: {
-            display: false,
-            position: "right",
-            align: "end",
-        }
-    };
-
-    // Chart options for year
-    let chartYearOptions = {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    suggestedMin: 100,
-                    suggestedMax: 200
-                },
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Weight (lbs)'
-                }
-            }],
-            xAxes: yearOption
-        },
-        legend: {
-            display: false,
-            position: "right",
-            align: "end",
-        }
-    };
 
     //Use this to set up initial cookie calls
     let currCookieValue = getCookie('chartType');
@@ -209,16 +239,20 @@ function createChart() {
     if(currCookieValue != null) {
         chartCookieValue = document.cookie.split('; ').find(row => row.startsWith('chartType')).split('=')[1];
     }
+    // Use the following to decide how far to shift weeks, months, or years
+    let moveWeeks = 0;
+    let moveMonths = 0;
+    let moveYears = 0;
     // Finds the last selected chart through cookie to stay on selected graph
-    let currOptions = chartOptions;
+    let currOptions = getWeekOptions(moveWeeks);
     if(chartCookieValue === 'week') {
-        currOptions = chartOptions;
+        currOptions = getWeekOptions(moveWeeks);
     }
     else if(chartCookieValue === 'month') {
-        currOptions = chartMonthOptions;
+        currOptions = getMonthOptions(moveMonths);
     }
     else if(chartCookieValue === 'year') {
-        currOptions = chartYearOptions;
+        currOptions = getYearOptions(moveYears);
     }
     // Creates chart data in HTML canvas
     let chart = new Chart(ctx, {
@@ -227,14 +261,14 @@ function createChart() {
         options: currOptions
     });
 
-    // Update charts according to button clicks
+    // Update charts according to week, month, and year clicks
     $("#week").click( function() {
         chart.destroy();
         document.cookie = "chartType=week;";
         chart = new Chart(ctx, {
             type: 'line',
             data: chartData,
-            options: chartOptions
+            options: getWeekOptions(moveWeeks)
         });
     });
     $("#month").click( function() {
@@ -243,7 +277,7 @@ function createChart() {
         chart = new Chart(ctx, {
             type: 'line',
             data: chartData,
-            options: chartMonthOptions
+            options: getMonthOptions(moveMonths)
         });
     });
     $("#year").click( function() {
@@ -252,9 +286,64 @@ function createChart() {
         chart = new Chart(ctx, {
             type: 'line',
             data: chartData,
-            options: chartYearOptions
+            options: getYearOptions(moveYears)
         });
     });
+
+    // Update charts according to left/right clicks
+    $("#left").click( function() {
+        chart.destroy();
+        let currCookieValue = getCookie('chartType');
+        let chartCookieValue; 
+        if(currCookieValue != null) {
+            chartCookieValue = document.cookie.split('; ').find(row => row.startsWith('chartType')).split('=')[1];
+        }
+        let thisOptions = getWeekOptions(moveWeeks);
+        if(chartCookieValue === 'week') {
+            moveWeeks--;
+            thisOptions = getWeekOptions(moveWeeks);
+        }
+        else if(chartCookieValue === 'month') {
+            moveMonths--;
+            thisOptions = getMonthOptions(moveMonths);
+        }
+        else if(chartCookieValue === 'year') {
+            moveYears--;
+            thisOptions = getYearOptions(moveYears);
+        }
+        chart = new Chart(ctx, {
+            type: 'line',
+            data: chartData,
+            options: thisOptions
+        })
+    });
+    $("#right").click( function() {
+        chart.destroy();
+        let currCookieValue = getCookie('chartType');
+        let chartCookieValue; 
+        if(currCookieValue != null) {
+            chartCookieValue = document.cookie.split('; ').find(row => row.startsWith('chartType')).split('=')[1];
+        }
+        let thisOptions = getWeekOptions(moveWeeks);
+        if(chartCookieValue === 'week') {
+            moveWeeks++;
+            thisOptions = getWeekOptions(moveWeeks);
+        }
+        else if(chartCookieValue === 'month') {
+            moveMonths++;
+            thisOptions = getMonthOptions(moveMonths);
+        }
+        else if(chartCookieValue === 'year') {
+            moveYears++;
+            thisOptions = getYearOptions(moveYears);
+        }
+        chart = new Chart(ctx, {
+            type: 'line',
+            data: chartData,
+            options: thisOptions
+        })
+
+    })
 
 
     // Registers click event for line graph
